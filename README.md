a8f0d70917423ff35a5274b67354dd827c60da70
kasun gamlath <kasun@kasuns-MacBook-Air.local>
2024-09-12 18:04:41 +0800

    working version

--- README.md
+++ README.md
@@ -1,1 +1,54 @@
 # caption_server
+
+/etc/nginx/nginx.conf
+```
+change the user to system user `ubuntu
+```
+
+/etc/systemd/system/caption.service (gunicorn service)
+```
+[Unit]
+Description=Gunicorn instance to serve caption
+After=network.target
+
+[Service]
+User=ubuntu
+Group=www-data
+WorkingDirectory=/home/ubuntu/caption_server
+Environment="PATH=/home/ubuntu/caption_server/venv/bin"
+<<<<<<< HEAD
+Environment="OPENAI_API_KEY=openai-key"
+=======
+Environment="OPENAI_API_KEY=openai_api_key"
+>>>>>>> 0f4027e (removed openai key)
+ExecStart=/home/ubuntu/caption_server/venv/bin/gunicorn --bind 0.0.0.0:5000 --workers 3 --bind unix:caption.sock -m 777 wsgi:app
+
+[Install]
+WantedBy=multi-user.target
+```
+
+/etc/nginx/sites-available/caption (nginx file)
+```
+server {
+    server_name 52.64.244.18 caption.kasungamlath.com;
+    location / {
+        include proxy_params;
+        proxy_pass http://unix:/home/ubuntu/caption_server/caption.sock;
+    }
+
+    listen 443 ssl; # managed by Certbot
+    ssl_certificate /etc/letsencrypt/live/caption.kasungamlath.com/fullchain.pem; # managed by Certbot
+    ssl_certificate_key /etc/letsencrypt/live/caption.kasungamlath.com/privkey.pem; # managed by Certbot
+    include /etc/letsencrypt/options-ssl-nginx.conf; # managed by Certbot
+    ssl_dhparam /etc/letsencrypt/ssl-dhparams.pem; # managed by Certbot
+}
+server {
+    if ($host = caption.kasungamlath.com) {
+        return 301 https://$host$request_uri;
+    } # managed by Certbot
+
+    listen 80;
+    server_name 52.64.244.18 caption.kasungamlath.com;
+    return 404; # managed by Certbot
+}
+```
--- caption.py
+++ caption.py
@@ -5,14 +5,7 @@ from openai import OpenAI
 
 client = OpenAI()
 
-# PROMPT = "you’re a professional photographer with a large follower base in Instagram. you want to increase the number of your followers. you are going to post the following photo in instagram. now take your time and think of a good instagram caption for this photo. don't include any emojis. don't be cheesy. try to maintain a professional tone. make the first sentence title. no need for special markup for title or newline."
-PROMPT = "Create a short, catchy, and engaging Instagram caption for this photo. The caption should reflect the mood of the photo and include an element of humor, inspiration, or a relatable statement. Incorporate relevant hashtags where necessary. don't include any emojis. don't enclose the caption in quotes."
-
-
-# Function to encode the image
-def encode_image(image_path):
-    with open(image_path, "rb") as image_file:
-        return base64.b64encode(image_file.read()).decode('utf-8')
+PROMPT = "Create a short, catchy, and engaging Instagram caption for this photo. The caption should reflect the mood of the photo and include an element of humor, inspiration, or a relatable statement. Incorporate relevant hashtags where necessary. don't enclose the caption in quotes."
 
 
 def get_caption_from_openai(base64_image):
@@ -39,7 +32,7 @@ def get_caption_from_openai(base64_image):
 
 
 app = Flask(__name__)
-CORS(app)
+CORS(app)  # required for testing on local host (if not for this it'll throw an error when both frontend and backend running on localhost)
 
 
 @app.route('/caption', methods=['POST'])
@@ -53,7 +46,6 @@ def upload_image():
     # Get the image file from the request
     image_file = request.json['image']
 
-    # Open the image using PIL
     caption = get_caption_from_openai(image_file)
 
     response = jsonify({'caption': caption})
